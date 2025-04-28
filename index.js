3811a38af3e8de4ef5ee24d5d8a89fc9bd82f30c
AhmedShebl2000 <bokashalaboka@gmail.com>
2025-04-28 15:58:19 +0300

    added user routes

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express();
 
 require("./startup/db")();
 require("./startup/routes")(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
