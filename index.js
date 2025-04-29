3d818678ba7c2ae092aa64ef3045076d1bcab15d
AhmedShebl2000 <bokashalaboka@gmail.com>
2025-04-30 01:37:45 +0300

    FIXES2

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+  })
+);
+
+require("./startup/prod")(app);
 require("./startup/db")();
 require("./startup/routes")(app);
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
