227e058624248a2405ede099784ffb4a2f52dc0b
Youssef <youssefyaser2020@gmail.com>
2025-04-30 03:02:02 +0300

    Installing 'cors' to  allow frontend-backend communication on different localhost ports

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
+const cors = require('cors');
+
 const express = require("express");
 const app = express();
+app.use(cors());
 
 require("./startup/db")();
 require("./startup/routes")(app);
