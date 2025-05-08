8c8f690591f8da13fab3877ce9159b4779723a90
AhmedShebl2000 <bokashalaboka@gmail.com>
2025-05-08 16:40:09 +0300

    added favorites-list for each user and added routes for them

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,59 @@ router.get("/", [auth, admin], async (req, res) => {
   }
 });
 
+// Get User Wishlist
+router.get("/wishlist", auth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId).select("wishlist");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const wishlist = user.wishlist;
+    if (wishlist.length === 0) {
+      return res.status(200).json({ message: "Wishlist is empty" });
+    } else {
+      return res.status(200).json({ wishlist });
+    }
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(400)
+      .json({ message: "Error fetching user wishlist", error });
+  }
+});
+
+// Add to user wishlist
+router.put("/wishlist", auth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const { gameId } = req.body;
+    if (!gameId) {
+      return res.status(400).json({ message: "Game ID is required" });
+    }
+    // Check if the gameId already exists in the wishlist
+    const gameExists = user.wishlist.includes(gameId);
+    if (gameExists) {
+      return res.status(400).json({ message: "Game already in wishlist" });
+    }
+    // Add the gameId to the wishlist
+    user.wishlist.push(gameId);
+    await user.save();
+    return res
+      .status(200)
+      .json({ message: "Game added to wishlist", wishlist: user.wishlist });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ message: "Error updating user wishlist.", error });
+  }
+});
+
 // Get user by id
 router.get("/:id", auth, async (req, res) => {
   const { id } = req.params;
@@ -36,18 +89,24 @@ router.get("/:id", auth, async (req, res) => {
 // add a user
 router.post("/", async (req, res) => {
   const { error } = validateUser(req.body);
-  if (error) return res.status(400).json({ errMessage: error.details[0].message });
+  if (error)
+    return res.status(400).json({ errMessage: error.details[0].message });
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).json({ errMessage: "User already registered." });
+  if (user)
+    return res.status(400).json({ errMessage: "User already registered." });
 
-  user = new User(_.pick(req.body, ["first_name", "last_name", "email", "password", "phone"]));
+  user = new User(
+    _.pick(req.body, ["first_name", "last_name", "email", "password", "phone"])
+  );
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   const token = user.generateAuthToken();
-  res.header("x-auth-token", token).send(_.pick(user, ["_id", "first_name", "last_name", "email", "phone"]));
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "first_name", "last_name", "email", "phone"]));
 });
 
 module.exports = router;
