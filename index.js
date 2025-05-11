5ae3864a3141658b96523143e663169ec1a8cfcd
AhmedShebl2000 <bokashalaboka@gmail.com>
2025-05-11 16:16:31 +0300

    for re-deploy

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ module.exports.handler = serverless(app);
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
-// });
+// /});
